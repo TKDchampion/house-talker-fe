76ec65bb75f2fcddaadfdc570c9cffa80f3336a2
TKDchampion <as1219123@yahoo.com.tw>
2022-01-01 09:34:06 +0800

    test logo and meta tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'house-talker-fe';
+  constructor(private meta: Meta) {
+    this.meta.addTag({
+      name: 'description',
+      content: 'This is an article about Angular Meta service',
+    });
+  }
 }
