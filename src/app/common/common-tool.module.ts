f054bd291ee506f8b49dae1ed15df99c6c0a42b3
TKDchampion <as1219123@yahoo.com.tw>
2022-01-08 17:21:44 +0800

    add list component

--- src/app/common/common-tool.module.ts
+++ src/app/common/common-tool.module.ts
@@ -3,10 +3,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AccordionComponent } from './accordion/accordion.component';
 import { ModalComponent } from './modal/modal.component';
+import { ListItemComponent } from './list-item/list-item.component';
 
 @NgModule({
-  declarations: [BoxComponent, AccordionComponent, ModalComponent],
+  declarations: [
+    BoxComponent,
+    AccordionComponent,
+    ModalComponent,
+    ListItemComponent,
+  ],
   imports: [CommonModule],
-  exports: [BoxComponent, AccordionComponent, ModalComponent],
+  exports: [
+    BoxComponent,
+    AccordionComponent,
+    ModalComponent,
+    ListItemComponent,
+  ],
 })
 export class CommonToolModule {}
