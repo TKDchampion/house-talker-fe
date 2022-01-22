c5d1d63d94a66ad22e7d50d4c787ce22e179ccc2
TKDchampion <as1219123@yahoo.com.tw>
2022-01-22 17:52:44 +0800

    basic form on article

--- src/app/pages/article-edit-create/article-edit-create.component.ts
+++ src/app/pages/article-edit-create/article-edit-create.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-article-edit-create',
   templateUrl: './article-edit-create.component.html',
-  styleUrls: ['./article-edit-create.component.scss']
+  styleUrls: ['./article-edit-create.component.scss'],
 })
 export class ArticleEditCreateComponent implements OnInit {
+  articleForm = this.fb.group({
+    title: [''],
+    description: [''],
+    cityName: ['', [Validators.required]],
+    tips: [''],
+  });
+  constructor(public fb: FormBuilder) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSubmit() {
+    console.log(this.articleForm.valid);
+
+    if (!this.articleForm.valid) {
+      alert('error');
+    }
   }
-
 }
--- src/app/pages/article-edit-create/article-edit-create.module.ts
+++ src/app/pages/article-edit-create/article-edit-create.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArticleEditCreateComponent } from './article-edit-create.component';
-import { ArticleEditCreateRoutingModule } from './home-routing.module';
+import { ArticleEditCreateRoutingModule } from './article-edit-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ArticleEditCreateComponent],
-  imports: [CommonModule, ArticleEditCreateRoutingModule],
+  imports: [CommonModule, ArticleEditCreateRoutingModule, ReactiveFormsModule],
 })
 export class ArticleEditCreateModule {}
